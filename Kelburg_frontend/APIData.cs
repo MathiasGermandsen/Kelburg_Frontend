d90f4f2f54f1854c772302b9d60f8bd02d1e2231
ArianYN <128600124+ArianYN@users.noreply.github.com>
2025-02-19 12:10:14 +0100

    Created APIHandler for ease-of-use between pages and API. + Imported Newtonsoft NuGet

---
+++ Kelburg_frontend/APIData.cs
@@ -0,0 +1,18 @@
+﻿namespace Kelburg_frontend;
+
+public class APIData
+{
+    protected readonly string _urlPath;
+
+    public APIData(string name, string urlPath)
+    {
+        Name = name;
+        _urlPath = urlPath;
+    }
+
+    public string Name { get; }
+
+    public string Create => $"{_urlPath}/create";
+    public string Read => $"{_urlPath}/read";
+    public string Delete => $"{_urlPath}/delete";
+}
---
+++ Kelburg_frontend/APIHandler.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace Kelburg_frontend;
+
+public class BookingsAPI : APIData
+{
+   public BookingsAPI(string urlPath) : base("Users", urlPath) {}
+
+   public string Update => $"{_urlPath}/update";
+}
+
+public class RoomsAPI : APIData
+{
+    public RoomsAPI(string urlPath) : base("Rooms", urlPath) {}
+    
+    public string ChangePrice => $"{_urlPath}/changePrice";
+}
+
+public class ServicesAPI : APIData
+{
+    public ServicesAPI(string urlPath) : base("Services", urlPath) {}
+    
+    public string createPredefinedServices => $"{_urlPath}/createPredefinedServices";
+    public string ChangePrice => $"{_urlPath}/changePrice";
+}
+
+public class UsersAPI : APIData
+{
+    public UsersAPI(string urlPath) : base("Users", urlPath) {}
+    
+    public string Login => $"{_urlPath}/login";
+}
+
+public static class eTables
+{
+    private const string APIBaseUrl = "https://localhost:44306/api";
+
+    public static readonly BookingsAPI Bookings = new($"{APIBaseUrl}/Bookings");
+    public static readonly APIData HotelCars = new ("HotelCars", $"{APIBaseUrl}/HotelCars");
+    public static readonly RoomsAPI Rooms = new($"{APIBaseUrl}/Rooms"); 
+    public static readonly ServicesAPI Services = new($"{APIBaseUrl}/Services");
+    public static readonly APIData Tickets = new("Tickets", $"{APIBaseUrl}/Tickets");
+    public static readonly UsersAPI Users = new($"{APIBaseUrl}/Users");
+}
+
+public static class APIHandler
+{
+    private static readonly HttpClient _httpClient = new HttpClient(
+        new HttpClientHandler { ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true }
+    );
+    public static async Task<T?> RequestAPI<T>(string apiUrl, Dictionary<string, object?> queryParams, HttpMethod method, object? body = null)
+    {
+        string queryParamString = string.Join("&", queryParams.Select(kvp => $"{HttpUtility.UrlEncode(kvp.Key)}={HttpUtility.UrlEncode(kvp.Value.ToString())}"));
+        string completeApiUrl = $"{apiUrl}?{queryParamString}";
+        
+        try
+        {
+            using HttpRequestMessage request = new HttpRequestMessage(method, completeApiUrl);
+            HttpResponseMessage response = await _httpClient.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+
+            string json = await response.Content.ReadAsStringAsync();
+            
+            return JsonConvert.DeserializeObject<T>(json);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"API Request Failed - {ex.Message}");
+            return default;
+        }
+    }
+}
