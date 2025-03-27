391bc1b9a1cd1f9e8364e9277e3ae06678796d90
Alexander Piilgaard <alex344b@edu.mercantec.dk>
2025-03-27 13:40:35 +0100

    nothing really

--- Kelburg_frontend/Components/Pages/AdminPages/AdminSelectedRoom.razor.cs
+++ Kelburg_frontend/Components/Pages/AdminPages/AdminSelectedRoom.razor.cs
@@ -3,15 +3,18 @@ using System.Security.Principal;
 using Microsoft.AspNetCore.Components;
 using Kelburg_frontend.Models;
 using Blazorise;
+using Kelburg_frontend.Services;
 
 namespace Kelburg_frontend.Components.Pages.AdminPages;
 
 public partial class AdminSelectedRoom : ComponentBase
 {
+    private IReadOnlyList<DateTime?> dateRange;
     private List<string> nextAvailableDates = new();
     private List<string> bookedDates = new();
-
-    private Rooms room;
+    
+    private Rooms room = new();
+    
     
     [Inject] private HttpClient Http { get; set; }
     
@@ -20,9 +23,24 @@ public partial class AdminSelectedRoom : ComponentBase
 
     protected override async Task OnInitializedAsync()
     {
+        Dictionary<string, object?> queryParams = new Dictionary<string, object?>()
+        {
+            {"startDate", dateRange[0].Value.ToString("yyyy-MM-dd")},
+            {"endDate", dateRange[1].Value.ToString("yyyy-MM-dd")},
+        };
+        
         try
         {
             room = await Http.GetFromJsonAsync<Models.Rooms>($"/api/room/{RoomId}");
+            
+            List<Models.Rooms> fetchedAvailableRooms = await APIHandler.RequestAPI<List<Models.Rooms>>(eTables.Rooms.AvailableBetweenDates, queryParams, HttpMethod.Get);
+      
+            List<Models.Rooms> bookedRooms = await APIHandler.RequestAPI<List<Models.Rooms>>(eTables.Rooms.UnavailableBetweenDates, queryParams, HttpMethod.Get);
+            
+            foreach (Rooms? room in bookedRooms)
+            {
+                room.isBooked = true;
+            }
             
             nextAvailableDates = await Http.GetFromJsonAsync<List<string>>("/api/room/next-available");
             bookedDates = await Http.GetFromJsonAsync<List<string>>("/api/room/booked");
