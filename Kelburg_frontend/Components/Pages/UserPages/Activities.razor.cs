427bfbcb9802f705f9ab84a4e871024e01f277d8
ArianYN <128600124+ArianYN@users.noreply.github.com>
2025-04-07 10:34:23 +0200

    Created Activities page + changed font for Mathias on career page because that dumbo forgot to do it

---
+++ Kelburg_frontend/Components/Pages/UserPages/Activities.razor.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Kelburg_frontend.Components.Pages.UserPages;
+
+public partial class Activities : ComponentBase
+{
+    
+    class Activity
+    {
+        public string Title { get; set; }
+        public string Image { get; set; }
+    }
+    
+    private List<Activity> activities = new()
+    {
+        new Activity { Title = "Tour de Kjellerup", Image =  "/images/activities/tour.png" },
+        new Activity { Title = "Water Gymnastics", Image = "/images/activities/water.png" },
+        new Activity { Title = "Morning Hike", Image =  "/images/activities/downhike.png" },
+        new Activity { Title = "Desert Camel Ride", Image = "/images/activities/camel.png" },
+        new Activity { Title = "Forest Horse Ride", Image = "/images/activities/horse.png" },
+        new Activity { Title = "Romantic Dinner", Image = "/images/activities/dinner.png" }
+    };
+    
+    bool showPopup = false;
+    string? selectedActivity = null;
+
+    void ShowPopup(string title)
+    {
+        selectedActivity = title;
+        showPopup = true;
+    }
+
+    void ClosePopup()
+    {
+        showPopup = false;
+        selectedActivity = null;
+    }
+}
